665e125b0123dcb96627617253aafe83f7b9cdd5
Luis J Camargo <lsjcp@yahoo.com>
2026-06-11 03:48:55 -0600

    import name collision

--- src/graphix_statevec_cuquantum/graphix_statevec_cuquantum.py
+++ src/graphix_statevec_cuquantum/graphix_statevec_cuquantum.py
@@ -20,7 +20,7 @@ from cuquantum.bindings import custatevec
 from graphix.parameter import Expression
 from graphix.sim.base_backend import DenseState, DenseStateBackend, Matrix
 from graphix.states import BasicStates, State
-from graphix.sim.statevec import Statevec
+from graphix.sim.statevec import Statevec as BaseStatevec
 
 if TYPE_CHECKING:
     from collections.abc import Sequence
@@ -93,7 +93,7 @@ class Statevec(DenseState):
         nqubit: int | None = None,
         max_space: int | None = None,
     ) -> None:
-        base = Statevec(data, nqubit)
+        base = BaseStatevec(data, nqubit)
 
         # Determine the actual max_space value
         actual_max_space: int
