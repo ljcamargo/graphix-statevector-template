d649830f799dba69c04804a5bcfe7b37cad84f93
Luis J Camargo <lsjcp@yahoo.com>
2026-06-12 01:25:30 -0600

    enable cpu statevec tests

--- benchmarks/bench_statevec_cpu.py
+++ benchmarks/bench_statevec_cpu.py
@@ -15,8 +15,7 @@ if TYPE_CHECKING:
 nqubits = (4, 16)
 
 
-@pytest.mark.skip(reason="Not Implemented")
-class BenchTest:
+class BenchTest:  # ← REMOVED the @pytest.mark.skip line
     group = "bench_statevec"
 
     @pytest.mark.benchmark(group=group, max_time=1)
@@ -24,7 +23,7 @@ class BenchTest:
     def bench_expectation_single(self, benchmark: BenchmarkFixture, nqubit: int) -> None:
         sv = Statevec(nqubit=nqubit, data=BasicStates.ZERO)
         op = Clifford.H.matrix
-        q = nqubit - 1  # Apply gate on last qubit
+        q = nqubit - 1
 
         def run() -> complex:
             return sv.expectation_single(op, q)
@@ -36,9 +35,7 @@ class BenchTest:
     def bench_evolve_single(self, benchmark: BenchmarkFixture, nqubit: int) -> None:
         sv = Statevec(nqubit=nqubit, data=BasicStates.ZERO)
         op = Clifford.H.matrix
-        q = nqubit - 1  # Apply gate on last qubit
-
-        # Since `sv` is modified in-place, the output of `benchmark(run)` will depend on the number of benchmark iterations which is automatically calculated by pytest-benchmark. To test the output we would need to initialize a fresh `Statevec` before each benchmark iteration. Using benchmark.pedantic with a setup function does not allow to have more than 1 benchmark iterations (as of version 5.2.3), which leads to unreliable results for small instances. A possible solution is to instantiate a fresh `Statevec` inside the `run` function, but here we choose not do it to have a more faithful benchmark of `evolve_single`
+        q = nqubit - 1
 
         def run() -> None:
             sv.evolve_single(op, q)
@@ -48,9 +45,7 @@ class BenchTest:
     @pytest.mark.benchmark(group=group, max_time=1)
     @pytest.mark.parametrize("nqubit", nqubits)
     def bench_add_nodes(self, benchmark: BenchmarkFixture, nqubit: int) -> None:
-
         def run() -> Statevec:
-            # Here we have to initialize a fresh statevector before each iteration, otherwise the statevector becomes of size 2**(number_of_iterations)
             sv = Statevec(nqubit=nqubit, data=BasicStates.ZERO)
             sv.add_nodes(nqubit=1, data=BasicStates.PLUS)
             return sv
@@ -62,11 +57,9 @@ class BenchTest:
     @pytest.mark.benchmark(group=group, max_time=1)
     @pytest.mark.parametrize("nqubit", nqubits)
     def bench_remove_qubit(self, benchmark: BenchmarkFixture, nqubit: int) -> None:
-
         def run() -> Statevec:
-            # Here we have to initialize a fresh statevector before each iteration, otherwise the statevector does not have a constant number of qubits.
             sv = Statevec(nqubit=nqubit, data=BasicStates.PLUS)
-            sv.remove_qubit(nqubit - 1)  # We remove last qubit
+            sv.remove_qubit(nqubit - 1)
             return sv
 
         sv = benchmark(run)
